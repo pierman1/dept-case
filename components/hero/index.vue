56c326cd98099ba557f3920a95a80b7fee0d4381
pierman1 <pbleeker@live.nl>
2020-03-03 23:48:29 +0100

    :boom: Layout, Breakpoints and Initial Hero

---
+++ components/hero/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="hero-large">
+    <div class="container">
+      <!-- <h1>
+        Work
+      </h1> -->
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+}
+</script>
+
+<style lang="scss" scoped>
+.hero-large {
+  display: flex;
+  position: relative;
+  width: 100%;
+  height: 375px;
+  background-image: url('~assets/images/florensis.jpg');
+  background-size: 100%;
+  background-repeat: no-repeat;
+
+  @include mq($from: wide) {
+    height: 940px;
+
+    &:before {
+      content: '';
+      position: absolute;
+      z-index: 9;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      box-shadow: 0 0 0 20px #fff inset;
+    }
+  }
+}
+</style>
