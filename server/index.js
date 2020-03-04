04663b7af432328f64a5e452f095adeb2801f4b2
pierman1 <pbleeker@live.nl>
2020-03-04 14:44:39 +0100

    :sparkles: Setup simple API
    API for consuming cases filtered

--- server/index.js
+++ server/index.js
@@ -3,27 +3,54 @@ const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 
+const cases = require('./mock-data/cases.json')
+
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
 
+const getCases = (query) => {
+  let result = cases
+  if (query.type) {
+    result = cases.filter(c => {
+      if (c.type.includes(query.type)) {
+        return c
+      }
+    })
+  }
+
+  if (query.industry) {
+    result = result.filter(c => {
+      if (c.industries.includes(query.industry)) {
+        return c
+      }
+    })
+  }
+
+  return result
+}
+
+app.get('/api/v1/cases', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'Cases retrieved successfully',
+    cases: getCases(req.query)
+  })
+});
+
 async function start() {
-  // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
   const { host, port } = nuxt.options.server
 
   await nuxt.ready()
-  // Build only in dev mode
   if (config.dev) {
     const builder = new Builder(nuxt)
     await builder.build()
   }
 
-  // Give nuxt middleware to express
   app.use(nuxt.render)
 
-  // Listen the server
   app.listen(port, host)
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
