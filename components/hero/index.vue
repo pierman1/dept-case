d55fd7ca9a3fe5dcf554bcfd55cc79f12a5419c7
pierman1 <pbleeker@live.nl>
2020-03-06 19:54:09 +0100

    :ambulance: Issue background hero

--- components/hero/index.vue
+++ components/hero/index.vue
@@ -116,16 +116,11 @@ export default {
     width: 100%;
     position: absolute;
     left: 50%;
-    top: 0;
-    transform: translateX(-50%);
+    top: 50%;
+    transform: translate(-50%, -50%);
 
     img {
-      height: 100%;
-
-      @include mq($from: tablet) {
-        width: 100%;
-        height: auto;
-      }
+      width: 100%;
     }
   }
 }
